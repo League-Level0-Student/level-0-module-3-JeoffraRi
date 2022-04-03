3427c12feb6b8aacfd52eae388ad2ff303c69a04
JamesQian123 <jamesqian12345678910@gmail.com>
2022-04-03 16:28:48 -0700

    4:28 PM Pacific Standard Time, April 3 2022
    The League

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -8,10 +8,17 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
+		rob.setX(100);
 		// You also need to show the robot to see the result of this line.
-
+        rob.show();
+        rob.penDown();
+        rob.setSpeed(25);
+        rob.turn(45);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+        for (int a = 0; a<5; a++) {
+        	rob.move(300);
+        	rob.turn(144);
+        }
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
