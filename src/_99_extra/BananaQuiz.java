3682cf79171147a9ddbc6b609d09ed3629978b04
League Student <league@iMac-24.attlocal.net>
2022-04-10 16:39:28 -0700

    sasasa
    ds

--- src/_99_extra/BananaQuiz.java
+++ src/_99_extra/BananaQuiz.java
@@ -9,6 +9,26 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String name = JOptionPane.showInputDialog(null, "What's your name?");
+		String doYou;
+		do {
+			 doYou = JOptionPane.showInputDialog(null, "Do you like bananas?");
+		if (doYou.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "What!? "+name+", you're crazy! No wonder you made that weird program last Sunday!");
+		}else if (doYou.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, ""+name+", you're a sensible person.");
+			String hobby = JOptionPane.showInputDialog(null, "<html>What's your hobby? <i>better not be not liking bananas</i></html>");
+			if (hobby.equalsIgnoreCase("not liking bananas")) {
+			JOptionPane.showMessageDialog(null, "<html>GAH! <i>WHY MUST YOU TORMENT ME!?!</i><html>");
+		}else {
+			JOptionPane.showMessageDialog(null, "Your hobby would be much better with bananas.");
+			JOptionPane.showMessageDialog(null, "<html>Oooh... "+hobby+" bananas... <i>I gotta try that sometime</i><html>");
+		}
+		}else {
+			JOptionPane.showMessageDialog(null, "What?");
+		}
+		
+		}while(!doYou.equalsIgnoreCase("yes")&&!doYou.equalsIgnoreCase("no"));
 		//2. if they say no, 
 			//tell them they are crazy 
 			//and end quiz
@@ -22,3 +42,49 @@ public class BananaQuiz
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/_99_extra/BirthdayReminder.java
+++ src/_99_extra/BirthdayReminder.java
@@ -1,19 +1,27 @@
 
 package _99_extra;
 
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
+
 
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+		
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "July 16th";
+		String dadsBirthday = "February 11th";
+		String myBirthday = "June 13th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	          JOptionPane.showMessageDialog(null,"STARTING UP...");
+	          playAsherSound();
+	          JOptionPane.showMessageDialog(null,"<html><i>BirthdaySearch Version 1.0</i></html>");
+	          String findName = JOptionPane.showInputDialog(null, "Who's birthday would you like to know? (Mom, Dad, me)");
 		// 3. Print out what the user typed
 		
 		// 4. if user asked for "mom"
@@ -23,6 +31,26 @@ public class BirthdayReminder {
 		// 6. if user asked for your name
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+	}
+
+	          public static void playAsherSound() {
+	      		String path = "src/_99_extra/sound.wav";
+	      
+	      			File sound = new File(path);
+	      			if (sound.exists()) {
+	      				try {					
+	      					Clip clip= AudioSystem.getClip();
+	      					clip.open(AudioSystem.getAudioInputStream(sound));
+	      					clip.start();
+	      					Thread.sleep(clip.getMicrosecondLength()/1000);
+	      				}
+	      				catch (Exception e) {
+	      					System.out.println("Could not play this sound");
+	      				}
+	      			}
+	      		
+	      	}
 
 	} 
-}
+
+//433930_devern_mac-os-startup-sound-alike.wav
