bf70cef29b0adc030591e500443b0d44b5781d0e
asherlaw <asherlaw@pepinos-air.lan>
2022-04-12 13:42:49 -0700

    gotta commit!

--- src/_99_extra/BirthdayReminder.java
+++ src/_99_extra/BirthdayReminder.java
@@ -1,4 +1,4 @@
-
+	
 package _99_extra;
 
 import java.io.File;
@@ -13,9 +13,9 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 		
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "July 16th";
-		String dadsBirthday = "February 11th";
-		String myBirthday = "June 13th";
+		String momsBirthday = "July 16th, 1978";
+		String dadsBirthday = "February 11th, 1968";
+		String myBirthday = "June 13th, 2011";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
 	          JOptionPane.showMessageDialog(null,"STARTING UP...");
@@ -23,7 +23,19 @@ public class BirthdayReminder {
 	          JOptionPane.showMessageDialog(null,"<html><i>BirthdaySearch Version 1.0</i></html>");
 	          String findName = JOptionPane.showInputDialog(null, "Who's birthday would you like to know? (Mom, Dad, me)");
 		// 3. Print out what the user typed
-		
+		if (findName.equalsIgnoreCase("mom")) {
+			JOptionPane.showMessageDialog(null, "PROCESSING...");
+			JOptionPane.showMessageDialog(null, "DATE FOUND.");
+			JOptionPane.showMessageDialog(null, ""+momsBirthday+"");
+		}else if (findName.equalsIgnoreCase("dad")) {
+			JOptionPane.showMessageDialog(null, "PROCESSING...");
+			JOptionPane.showMessageDialog(null, "DATE FOUND.");
+			JOptionPane.showMessageDialog(null, ""+dadsBirthday+"");
+		} else if (findName.equalsIgnoreCase("you")) {
+			JOptionPane.showMessageDialog(null, "PROCESSING...");
+			JOptionPane.showMessageDialog(null, "DATE FOUND.");
+			JOptionPane.showMessageDialog(null, ""+myBirthday+"");
+		}
 		// 4. if user asked for "mom"
 			//print mom's birthday
 		// 5. if user asked for "dad"
@@ -54,3 +66,71 @@ public class BirthdayReminder {
 	} 
 
 //433930_devern_mac-os-startup-sound-alike.wav
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/_99_extra/DizzyRobot.java
+++ src/_99_extra/DizzyRobot.java
@@ -1,16 +1,22 @@
 
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
+		robot.setSpeed(50);
 		
-		// 1. Use the dance method below to make the robot spin.		
+		// 1. Use the dance method below to make the robot spin.
+		dance(5);
 		// 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-	
-
+		String dizzy = JOptionPane.showInputDialog(null, "How dizzy should this robot be on a scale of one to ten?");
+		int dizzyInt = Integer.parseInt(dizzy);
+		dance(dizzyInt);
+		JOptionPane.showMessageDialog(null, "<html><i>\"Ughhhh....\" *vomit*</i></html>");
 	}
 
 	/*****************   Use this method, DON'T CHANGE THE CODE BELOW  **************/
